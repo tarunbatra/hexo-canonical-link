daa1cac80481f5452b3748d8dedd3bfedc49ebc4
Tarun Batra <tarun@smallcase.com>
2019-07-31 01:56:56 +0530

    Stable release

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hexo-canonical-link
-Hexo plugin to add canonical link in articles
+Hexo plugin to add canonical links for pages
+
+## Usage in ejs
+
+```ejs
+<%- canonicalLink(config, page) %>
+```
+
+By default, the url of the page will be set as canonical link. To override this, provide `canonical_url` in the front matter of the page.
+
+## License
+MIT
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hexo.extend.helper.register('canonicalLink', (page) => `<link rel="canonical" href="${page.canonical_url.replace('index.html', '').toLowerCase()} + "/>`);
+'use strict';
+
+hexo.extend.helper.register('canonicalLink', (config, page) => {
+    let canonical_url = page.canonical_url;
+    if (!canonical_url) {
+        var base_url = config.url;
+        if (config.url.charAt(config.url.length - 1) !== '/') base_url += '/';
+        canonical_url = (page.canonical_url || base_url + page.canonical_path).replace('index.html', '').toLowerCase()
+    }
+    return `<link rel="canonical" href="${canonical_url}"/>`;
+});
