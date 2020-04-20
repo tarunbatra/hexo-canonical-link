f4e3abda6e7d6452e8d85c56823881877a9d4a87
Tarun Batra <tarun@smallcase.com>
2020-04-20 22:39:38 +0530

    BREAKING: Do not lowercase the url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ hexo.extend.helper.register('canonicalLink', (config, page) => {
     if (!canonical_url) {
         var base_url = config.url;
         if (config.url.charAt(config.url.length - 1) !== '/') base_url += '/';
-        canonical_url = (page.canonical_url || base_url + page.canonical_path).replace('index.html', '').toLowerCase()
+        canonical_url = (base_url + page.canonical_path).replace('index.html', '')
     }
     return `<link rel="canonical" href="${canonical_url}"/>`;
 });
